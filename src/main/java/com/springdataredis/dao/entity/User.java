2780b8ffddbcad90566e7895f0d93b35d102a2af
Shalale <selale.memmedli11@gmail.com>
2023-01-08 19:31:12 +0400

    docker ingeration

--- src/main/java/com/springdataredis/dao/entity/User.java
+++ src/main/java/com/springdataredis/dao/entity/User.java
@@ -12,6 +12,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Entity
@@ -19,7 +20,9 @@ import java.time.LocalDateTime;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
